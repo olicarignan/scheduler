e27e3b540ecba9a8853b2a197a5662371de5afdf
Olivier <olive.carignan@gmail.com>
2019-10-28 19:56:49 -0400

    form Appointment

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -160,4 +161,18 @@ storiesOf("InterviewerList", module)
   .add("Error", () => (
     <Error message="Could not delete appointment."
            onClose={action("onClose")} />
+  ))
+  .add("Form Edit", () => (
+    <Form name="Olivier Carignan"
+          interviewers={interviewers}
+          interviewer={interviewer.id}
+          onSave={action("onSave")}
+          onCancel={action("onCancel")}
+           />
+  ))
+  .add("Form Create", () => (
+    <Form interviewers={interviewers}
+          onSave={action("onSave")}
+          onCancel={action("onCancel")} 
+          />
   ))
