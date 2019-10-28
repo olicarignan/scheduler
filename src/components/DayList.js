bccd68f8e65ae278826d37458ef7359f4222c44d
Olivier <olive.carignan@gmail.com>
2019-10-28 16:36:22 -0400

    implement hook

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classnames from 'classnames';
+import DayListItem from "components/DayListItem.js";
+
+export default function DayList(props) {
+  let days = props.days.map(day => {
+    return (
+      <DayListItem
+      key={day.id}
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}
+      />
+    );
+  });
+  return days;
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,12 @@
 import React from "react";
-import classnames from 'classnames';
+import classnames from "classnames";
 import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
 
   const dayClass = classnames("day-list__item", {
-    "--selected": (props.selected === true),
-    "--full.": (props.spot === 0)
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": (props.spots === 0)
   });
 
   const formatSpots = (item) => {
@@ -21,7 +21,10 @@ export default function DayListItem(props) {
 
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li 
+        className={dayClass} 
+        onClick={() => props.setDay(props.name)}
+        >
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
