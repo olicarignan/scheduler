40fd31585dceeb29de88700e4bc1af6527dd8f49
Olivier <olive.carignan@gmail.com>
2019-10-28 17:51:31 -0400

    interviewer list + interviewer list item

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classnames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected" : props.selected,
+  })
+
+  const interviewerImgClass = classnames("interviewers__item-image", {
+    "interviewers__item-image--selected" : props.selected
+  });
+
+
+  return (
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
