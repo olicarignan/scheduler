f4b556003f3affa091886936aea3c736cd00a955
Olivier <olive.carignan@gmail.com>
2019-10-29 13:32:42 -0400

    displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,15 @@
 import "components/Appointment/styles.scss";
-import React from "react";
+import React, {useState} from "react";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+        {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+        {!props.interview && <Empty></Empty>}
+    </article>
   )
 }
